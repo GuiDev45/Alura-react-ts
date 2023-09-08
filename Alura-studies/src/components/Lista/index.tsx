8ea61bbec4e050861b8ad401ca901740742ad8d4
GuiDev45 <bortolettodev@gmail.com>
2023-09-08 16:24:37 -0300

    Criando a lista

---
+++ Alura-studies/src/components/Lista/index.tsx
@@ -0,0 +1,20 @@
+// Forma mais nova de criar um componente é utilizando a function
+function Lista() {
+  return (
+    <aside>
+      <h2> Estudos do dia </h2>
+      <ul>
+        <li>
+          <h3>React</h3>
+          <span>02:00:00</span>
+        </li>
+        <li>
+          <h3>JavaScript</h3>
+          <span>01:00:00</span>
+        </li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Lista;
