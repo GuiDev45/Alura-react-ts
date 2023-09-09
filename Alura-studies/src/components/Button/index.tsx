588a14153a3441412eeaf01012c37c6f766ec70c
GuiDev45 <bortolettodev@gmail.com>
2023-09-09 18:27:17 -0300

    007 - Alterando nome do style

--- Alura-studies/src/components/Button/index.tsx
+++ Alura-studies/src/components/Button/index.tsx
@@ -1,7 +1,9 @@
-import style from "./Botao.module.scss";
+import botao from "./Botao.module.scss";
 
 const Botao = () => {
-  return <button className={style.botao}>Botão</button>;
+  return <button className={botao.botao}>Botão</button>;
 };
 
 export default Botao;
+
+// Não precisa usar exatamente style, qualquer nome dado no import funciona.
