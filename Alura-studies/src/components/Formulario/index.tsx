b835e05fcce67b194a9765c99d5a23bc69aad57f
GuiDev45 <bortolettodev@gmail.com>
2023-09-09 21:58:30 -0300

    012 - Controlando o formulário

--- Alura-studies/src/components/Formulario/index.tsx
+++ Alura-studies/src/components/Formulario/index.tsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import Botao from "../Button";
 import style from "./Formulario.module.scss";
 
 const Formulario = () => {
+  const [state, setState] = useState({
+    tarefa: "",
+    tempo: "00:00",
+  });
+
+  const adicionarTarefa = (evento: React.FormEvent) => {
+    evento.preventDefault();
+    console.log("state: ", state);
+  };
+
   return (
-    <form className={style.novaTarefa}>
+    <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
       <div className={style.inputContainer}>
         <label htmlFor="tarefa">Adicione um novo estudo</label>
         <input
           type="text"
           name="tarefa"
+          value={state.tarefa}
+          onChange={(evento) =>
+            setState({ ...state, tarefa: evento.target.value })
+          }
           id="tarefa"
           placeholder="O que você quer estudar"
           required
@@ -20,6 +35,10 @@ const Formulario = () => {
           type="time"
           step="1"
           name="tempo"
+          value={state.tempo}
+          onChange={(evento) =>
+            setState({ ...state, tempo: evento.target.value })
+          }
           id="tempo"
           min="00:00:00"
           max="01:30:00"
