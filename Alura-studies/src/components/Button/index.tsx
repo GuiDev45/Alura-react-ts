505bf9834b5040aa05b9792103ee55f4e08d9b30
GuiDev45 <bortolettodev@gmail.com>
2023-09-14 11:54:51 -0300

    Regressiva

--- Alura-studies/src/components/Button/index.tsx
+++ Alura-studies/src/components/Button/index.tsx
@@ -3,11 +3,12 @@ import botao from "./Botao.module.scss";
 type botaoProps = {
   children: React.ReactNode;
   type?: "button" | "submit" | "reset" | undefined;
+  onClick?: () => void;
 };
 
-const Botao = ({ children, type }: botaoProps) => {
+const Botao = ({ children, type, onClick }: botaoProps) => {
   return (
-    <button type={type} className={botao.botao}>
+    <button onClick={onClick} type={type} className={botao.botao}>
       {children}
     </button>
   );
