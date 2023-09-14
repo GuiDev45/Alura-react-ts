1cb44d2e44a2d71d65ab8195b74ef4497e671f84
GuiDev45 <bortolettodev@gmail.com>
2023-09-13 22:30:29 -0300

    015 - Selecionando uma tarefa

--- Alura-studies/src/pages/App.tsx
+++ Alura-studies/src/pages/App.tsx
@@ -7,11 +7,16 @@ import { ITarefa } from "../types/tarefa";
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+  }
 
   return (
     <div className={sytle.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas} />
+      <Lista tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
       <Cronometro />
     </div>
   );
